751cda0273eb674587c4c291c822c951980d5b66
Arkadiusz Koszewski <akoszews@gmail.com>
2026-06-28 23:28:50 +0000

    Copy debug APK to artifacts

--- build.sh
+++ build.sh
@@ -7,3 +7,5 @@ export JAVA_HOME=/opt/android-studio/jbr
 export PATH=/home/debian/Android/Sdk/platform-tools:/home/debian/Android/Sdk/cmdline-tools/latest/bin:/home/debian/.local/bin:/usr/local/bin:/usr/bin:/bin
 
 gradle assembleDebug
+mkdir -p "$HOME/Artifacts/android"
+cp app/build/outputs/apk/debug/app-debug.apk "$HOME/Artifacts/android/SshApkDownloader-debug.apk"
